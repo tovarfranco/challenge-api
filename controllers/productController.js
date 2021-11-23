dd206ee385c110a4cc6b1b760be559791862e011
Franco Tovar <franco.tovar.ext@kavak.com>
2021-11-23 16:12:51 -0300

    feat : first version

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,9 +3,7 @@ const fs = require('fs');                                                       
 const path = require('path'); 
 
 // =========== Read BBDD ===========================
-let productListPath = path.join(__dirname, "../database/productsList.json");    // Ruta del archivo BBDD.
-const products = JSON.parse(fs.readFileSync(productListPath, 'utf-8'));
-let productList = products["products"]
+
 
 
 let discount = (product, discount) =>{
@@ -41,60 +39,32 @@ let calculateDiscount = (products) => {
     return products
 
 }
-// productList.forEach(product => {
-//     if (product.category == 'boots'){
-//         discount(product, 0.3)
-//     }
-//     else if(product.sku == 000003){
-//         discount(product, 0.15)
-//     }
-//     else{
-//         no_discount(product)
-//     }
-// });
 
-
-// let filterProductsBy = (productList, param) => {
-//     return productList.filter(product => {
-//         return product[Object.keys(param)] == param[Object.keys(param)]
-//     });
-// }
 
 // =========== Controller ============================
 const productController = {
     index: (req,res) => {
-
-        // if(Object.keys(req.query).length == 0){
-        //     res.send(productList)
-        // }
-        // else{
-        //     if (req.query){
-        //         let filteredList = filterProductsBy(productList, req.query)
-        //         res.send(filteredList)
-        //     }
-        //     else{
-        //         filteredList = productList
-        //         res.send(filteredList)
-        //     }
-        // }
-
+        let productListPath = path.join(__dirname, "../database/productsList.json");    // Ruta del archivo BBDD.
+        const products = JSON.parse(fs.readFileSync(productListPath, 'utf-8'));
+        const productList = products["products"]
+        
         let filteredProducts = []
 
         if (req.query.category){
             filteredProducts = productList.filter((product) => {
                 return product.category == req.query.category
             })
-            res.send(filteredProducts)
+            res.send(calculateDiscount(filteredProducts))
         }
         else if (req.query.priceLessThan){
             filteredProducts = productList.filter((product) => {
                 return product.price < req.query.priceLessThan
             })
-            res.send(filteredProducts)
+            res.send(calculateDiscount(filteredProducts))
         }
         else{
             filteredProducts = productList
-            res.send(filteredProducts)
+            res.send(calculateDiscount(filteredProducts))
         }
     }
 }
