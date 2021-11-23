cd20c06bc1a8f8e9629194a567106e6a96cf30c7
Franco Tovar <franco.tovar.ext@kavak.com>
2021-11-23 09:37:54 -0300

    feat : requirements and routes

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var express = require('express')
+// =========== General ===================================
+const express = require('express');                                   
+const path = require('path');   
+
+// =========== Express app ===================================
+const app = express()
+
+// =========== Routing ===================================
+const productRoute = require('./routes/productRoute.js');
+
+app.use('/products', productRoute);
