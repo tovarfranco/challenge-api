aa2158ad4d50fd47e9c28d9cfc706bc1a8f32dc3
Franco Tovar <franco.tovar.ext@kavak.com>
2021-11-23 15:30:10 -0300

    feat : work checkpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,27 +4,43 @@ const path = require('path');
 
 // =========== Read BBDD ===========================
 let productListPath = path.join(__dirname, "../database/productsList.json");    // Ruta del archivo BBDD.
-let products = JSON.parse(fs.readFileSync(productListPath, 'utf-8'));
+const products = JSON.parse(fs.readFileSync(productListPath, 'utf-8'));
 let productList = products["products"]
 
-// let discount = (product, discount) =>{
-//     product.price = {
-//         original : product.price,
-//         final : product.price * discount,
-//         discount_percentage: (discount*100).toString() + '%',
-//         currency: "EUR"
-//     }
-// }
 
-// let no_discount = (product) =>{
-//     product.price = {
-//         original : product.price,
-//         final : product.price,
-//         discount_percentage: null,
-//         currency: "EUR"
-//     }
-// }
+let discount = (product, discount) =>{
+    product.price = {
+        original : product.price,
+        final : product.price * discount,
+        discount_percentage: (discount*100).toString() + '%',
+        currency: "EUR"
+    }
+}
 
+let no_discount = (product) =>{
+    product.price = {
+        original : product.price,
+        final : product.price,
+        discount_percentage: null,
+        currency: "EUR"
+    }
+}
+
+let calculateDiscount = (products) => {
+    products.forEach(product => {
+        if (product.category == 'boots'){
+            discount(product, 0.3)
+        }
+        else if(product.sku == 000003){
+            discount(product, 0.15)
+        }
+        else{
+            no_discount(product)
+        }
+    })
+    return products
+
+}
 // productList.forEach(product => {
 //     if (product.category == 'boots'){
 //         discount(product, 0.3)
@@ -61,21 +77,23 @@ const productController = {
         //         res.send(filteredList)
         //     }
         // }
-        
+
+        let filteredProducts = []
+
         if (req.query.category){
-            let filteredProducts = productList.filter((product) => {
+            filteredProducts = productList.filter((product) => {
                 return product.category == req.query.category
             })
             res.send(filteredProducts)
         }
         else if (req.query.priceLessThan){
-            let filteredProducts = productList.filter((product) => {
+            filteredProducts = productList.filter((product) => {
                 return product.price < req.query.priceLessThan
             })
             res.send(filteredProducts)
         }
         else{
-            let filteredProducts = productList
+            filteredProducts = productList
             res.send(filteredProducts)
         }
     }
