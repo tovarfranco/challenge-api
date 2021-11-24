94505cf69c86ef30e6672dfe61f0fb1bd7e30a50
Franco Tovar <franco.tovar.ext@kavak.com>
2021-11-24 15:18:37 -0300

    feat : swagger doc

--- app.js
+++ app.js
@@ -7,9 +7,35 @@ const app = express()
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// =========== Swagger Docs ===================================
+const swaggerDocs = require('./docs/swagger.js')
+const swaggerUi = require('swagger-ui-express')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 // =========== Routing ===================================
 const productsRoute = require('./routes/productsRoute.js');
 
+/**
+ * @swagger
+ * /products:
+ *  get:
+ *      description: Use to request all products
+ *      parameters:
+ *       - in: query
+ *         name: category
+ *         type: string
+ *         required: false
+ *         description: description of parameter
+ *       - in: query
+ *         name: priceLessThan
+ *         type: string
+ *         required: false
+ *         description: description of parameter
+ *      responses: 
+ *          '200':
+ *              description: A successfull response
+ */
 app.use('/products', productsRoute);
 
 
